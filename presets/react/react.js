be352df555879687d98c611815baf843b7d920ed
Michał Sobiecki <michal.sobiecki@pracuj.pl>
2023-07-10 19:44:37 +0200

    feat: init config

--- presets/react/react.js
+++ presets/react/react.js
@@ -2,6 +2,15 @@ const getTsConfig = require("../../utils/get-ts-config");
 
 module.exports = {
   extends: ["airbnb", "airbnb/hooks"],
+  rules: {
+    "react/function-component-definition": [
+      "error",
+      {
+        namedComponents: ["arrow-function"],
+        unnamedComponents: ["arrow-function"],
+      },
+    ],
+  },
   overrides: [
     {
       files: ["**/*.ts?(x)"],
