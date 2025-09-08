60f943d5124b7b612df2722cf4317f5c19584bf9
Michał Sobiecki <michal.sobiecki@pracuj.pl>
2025-09-08 13:22:11 +0200

    Remove custom React function component rules

--- presets/next/next.js
+++ presets/next/next.js
@@ -2,15 +2,6 @@ const getTsConfig = require("../../utils/get-ts-config");
 
 module.exports = {
   extends: ["next"],
-  rules: {
-    "react/function-component-definition": [
-      "error",
-      {
-        namedComponents: ["arrow-function"],
-        unnamedComponents: ["arrow-function"],
-      },
-    ],
-  },
   overrides: [
     {
       files: ["**/*.ts?(x)"],
