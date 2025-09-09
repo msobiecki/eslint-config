b1636abae3b4f7c733bb077c594b491f04f4e301
Michał Sobiecki <michalsobiecki@yahoo.com>
2025-09-09 20:40:44 +0200

    feat: update

--- presets/react/react.js
+++ presets/react/react.js
@@ -31,5 +31,38 @@ module.exports = {
         ],
       },
     },
+    {
+      files: [
+        "contexts/**/*.{js,jsx,ts,tsx}",
+        "components/**/*.{js,jsx,ts,tsx}",
+      ],
+      rules: {
+        "unicorn/filename-case": [
+          "error",
+          {
+            cases: {
+              pascalCase: true,
+            },
+          },
+        ],
+      },
+    },
+    {
+      files: ["**/*.{js,jsx,ts,tsx}"],
+      excludedFiles: [
+        "contexts/**/*.{js,jsx,ts,tsx}",
+        "components/**/*.{js,jsx,ts,tsx}",
+      ],
+      rules: {
+        "unicorn/filename-case": [
+          "error",
+          {
+            cases: {
+              kebabCase: true,
+            },
+          },
+        ],
+      },
+    },
   ],
 };
