a2eefd356940d535c9423b91e73b5135e5e36986
Michał Sobiecki <michalsobiecki@yahoo.com>
2025-10-20 20:05:23 +0200

    feat: update node

--- rules/node.js
+++ rules/node.js
@@ -7,6 +7,12 @@ const nodeRules = {
 
   // disallow string concatenation with __dirname and __filename
   "n/no-path-concat": "error",
+
+  // disable no-missing-import rule as it is handled by import plugin
+  "n/no-missing-import": "off",
+
+  // disable n/no-missing-require rule as it is handled by import plugin
+  "n/no-missing-require": "off",
 };
 
 export default nodeRules;
